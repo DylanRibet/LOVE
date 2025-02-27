72c0a5293c966b70995eb6c70a4d041ff4fa82a2
DylanRoca <dylan.ribet@gmail.com>
2025-02-26 19:16:51 -0500

    v3
    v

--- server.py
+++
@@ -1,9 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-
-PORT = 8000
-
-# Créer un serveur HTTP simple
-with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-    print(f"Serveur démarré sur le port {PORT}. Ouvrez http://localhost:{PORT} dans votre navigateur.")
-    httpd.serve_forever()
